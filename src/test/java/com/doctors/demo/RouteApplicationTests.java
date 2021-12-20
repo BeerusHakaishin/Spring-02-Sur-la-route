76780f90f99e224c79248d185583448dd1ccaf7c
Ismaël Meziani <meziani-i@outlook.fr>
2021-12-20 11:34:34 +0100

    all good

---
+++ src/test/java/com/doctors/demo/RouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctors.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
